2ffde22fc2cfe1f85ab246b98ab9d24f1c127ba7
wangqiang <wangqiang@e.hunantv.com>
2015-11-11 20:01:39 +0800

    add mysql

---
+++ Mysql/PhpMysql.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Mysql单例模型
+ * Mysql连接
+ *
+ * @author by wangqiang <wangqiang@e.hunantv.com>
+ */
+namespace Mysql;
+
+/**
+ * Mysql连接
+ *
+ * Mongodb\Connect
+ * \Mongodb\Connect::getInstance()->connect($config)
+ */
+class PhpMysql
+{
+    private $_dsns = array();
+    private static $_instance = null; 
+
+    /**
+     * 构造函数
+     */
+    private function __construct()
+    {
+        //pass
+    }
+
+    /**
+     * 单列模式
+     *
+     * @return \Mongodb\Connect
+     */
+    public static function getInstance()
+    {
+        if (!is_null(self::$_instance)) {
+            return self::$_instance;
+        }
+
+        self::$_instance = new self();
+        return self::$_instance;
+    }
+    /**
+     * 连接Mysql
+     *
+     * @param array  $config 
+     * array('host'=>'', 'dbName'=>'', 'user'=>'','pass'=>'')
+     *
+     * @return \PDO
+     */
+    public function connect(array $config = array())
+    {
+        $dsn = "mysql:dbname={$config['dbName']};host={$config['host']}";
+        $md5 = md5($dsn);
+        if (empty($this->_dsns[$md5])) {
+            $this->_dsns[$md5] = new PDO($dsn, $config['user'], $config['pass']);
+        }
+
+        return $this->_dsns[$md5];
+    }
+}
+
+
